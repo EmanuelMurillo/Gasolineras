7dbaee7c7cb9d2a72389c63b53733803eda88e54
francisco.emanuel.murillo@gmail.com <francisco.emanuel.murillo@gmail.com>
2022-05-03 14:06:48 -0700

    Vistas

---
+++ Gasolineras/Controllers/AmenidadesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gasolineras.Models;
+
+namespace Gasolineras.Controllers
+{
+    public class AmenidadesController : Controller
+    {
+        private GasolinerasEntities1 db = new GasolinerasEntities1();
+
+        // GET: Amenidades
+        public ActionResult Index()
+        {
+            return View(db.Amenidades.ToList());
+        }
+
+        // GET: Amenidades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Amenidade amenidade = db.Amenidades.Find(id);
+            if (amenidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(amenidade);
+        }
+
+        // GET: Amenidades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Amenidades/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion")] Amenidade amenidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Amenidades.Add(amenidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(amenidade);
+        }
+
+        // GET: Amenidades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Amenidade amenidade = db.Amenidades.Find(id);
+            if (amenidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(amenidade);
+        }
+
+        // POST: Amenidades/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion")] Amenidade amenidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(amenidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(amenidade);
+        }
+
+        // GET: Amenidades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Amenidade amenidade = db.Amenidades.Find(id);
+            if (amenidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(amenidade);
+        }
+
+        // POST: Amenidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Amenidade amenidade = db.Amenidades.Find(id);
+            db.Amenidades.Remove(amenidade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Gasolineras/Controllers/GasolinerasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gasolineras.Models;
+
+namespace Gasolineras.Controllers
+{
+    public class GasolinerasController : Controller
+    {
+        private GasolinerasEntities1 db = new GasolinerasEntities1();
+
+        // GET: Gasolineras
+        public ActionResult Index()
+        {
+            var gasolineras = db.Gasolineras.Include(g => g.Marca1);
+            return View(gasolineras.ToList());
+        }
+
+        // GET: Gasolineras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gasolinera gasolinera = db.Gasolineras.Find(id);
+            if (gasolinera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gasolinera);
+        }
+
+        // GET: Gasolineras/Create
+        public ActionResult Create()
+        {
+            ViewBag.Marca = new SelectList(db.Marcas, "Id", "Descripcion");
+            return View();
+        }
+
+        // POST: Gasolineras/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Latitud,Longitud,Nombre,Marca,Amenidades,PrecioMagna,PrecioPremium,PrecioDiesel,HoraApertura,HoraCierre")] Gasolinera gasolinera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gasolineras.Add(gasolinera);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Marca = new SelectList(db.Marcas, "Id", "Descripcion", gasolinera.Marca);
+            return View(gasolinera);
+        }
+
+        // GET: Gasolineras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gasolinera gasolinera = db.Gasolineras.Find(id);
+            if (gasolinera == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Marca = new SelectList(db.Marcas, "Id", "Descripcion", gasolinera.Marca);
+            return View(gasolinera);
+        }
+
+        // POST: Gasolineras/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Latitud,Longitud,Nombre,Marca,Amenidades,PrecioMagna,PrecioPremium,PrecioDiesel,HoraApertura,HoraCierre")] Gasolinera gasolinera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gasolinera).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Marca = new SelectList(db.Marcas, "Id", "Descripcion", gasolinera.Marca);
+            return View(gasolinera);
+        }
+
+        // GET: Gasolineras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gasolinera gasolinera = db.Gasolineras.Find(id);
+            if (gasolinera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gasolinera);
+        }
+
+        // POST: Gasolineras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gasolinera gasolinera = db.Gasolineras.Find(id);
+            db.Gasolineras.Remove(gasolinera);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
